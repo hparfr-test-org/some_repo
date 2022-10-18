cb3cbd7f338c3f745bec99f214518971d1ce546f
Hpar <raph@hpar.fr>
2022-10-18 23:54:17 +0200

    Create __manifest__.py

---
+++ module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Some module',
+    'version': '14.0.0.0.1',
+    'author': 'Akretion',
+    'website': 'www.akretion.com',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules',
+    'description': """
+Do nothing""",
+    'depends': [
+        'sale',
+    ],
+    'installable': True,
+    'application': False,
+}
